fc7ac24a55af606986ac1265df77535a45495973
Mariano V <marianov994@gmail.com>
2019-12-16 02:33:39 -0300

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React, { FunctionComponent } from 'react';
 import './App.scss';
+import CardHeader from './components/dumb/CardHeader/CardHeader';
 
 const App: FunctionComponent = () => {
   return (
     <div className="container">
-      <div></div>
+      {/* <Select
+        onSelect={(value: any) => console.log(value)}
+        items={[
+          { title: 'bitcoin', value: 'bitcoin' },
+          { title: 'ethereum', value: 'ethereum' }
+        ]}
+      /> */}
+      <CardHeader content={'+$3415'} subtitle={'-BTC0,35415'} className="operations" />
     </div>
   );
 };
