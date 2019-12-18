cdaa4153a5204c343c81cb6a5d529f08eb27ba81
Mariano V <marianov994@gmail.com>
2019-12-18 15:08:47 -0300

    component updates

--- src/components/dumb/CurrencyStatus/CurrencyStatus.tsx
+++ src/components/dumb/CurrencyStatus/CurrencyStatus.tsx
@@ -17,14 +17,12 @@ const CurrencyStatus: FunctionComponent<IProps> = props => {
   const { principalValue, secondaryValue, changeHelper, onClick } = props;
 
   return (
-    <div className="temporal--container">
-      <div id="currency-status" onClick={() => (onClick ? onClick() : defaultProps.onClick())}>
-        {changeHelper && <CardHeader subtitle="Click to change" icon={exchange} className="currency-status" />}
-        <div className="principal">
-          <CardHeader content={principalValue} icon={bitcoin} className="text-big" />
-        </div>
-        <CardHeader content={secondaryValue} className="currency-status" />
+    <div id="currency-status" onClick={() => (onClick ? onClick() : defaultProps.onClick())}>
+      {changeHelper && <CardHeader subtitle="Click to change" icon={exchange} className="currency-status" />}
+      <div className="principal">
+        <CardHeader content={principalValue} icon={bitcoin} className="text-big" />
       </div>
+      <CardHeader content={secondaryValue} className="currency-status" />
     </div>
   );
 };
