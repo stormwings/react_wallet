a15d5b1ee94a27f53de9fc41aa8f1d8d93218a36
Mariano Villarreal <marianov994@gmail.com>
2019-12-22 05:21:46 -0300

    new routes to trading

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,6 +6,9 @@ import Auth from './components/view/Auth/Auth';
 import Charge from './components/view/Charge/Charge';
 import History from './components/view/History/History';
 import User from './components/view/User/User';
+import TradingList from './components/view/Trading/List';
+import TradingPublish from './components/view/Trading/Publish';
+import TradingBuy from './components/view/Trading/Buy';
 
 const Router: FunctionComponent<BrowserRouterProps> = () => {
   return (
@@ -16,7 +19,9 @@ const Router: FunctionComponent<BrowserRouterProps> = () => {
             <Route exact path="/" component={Dashboard} />
             <Route exact path="/auth/:type" component={Auth} />
             <Route exact path="/charge/:type" component={Charge} />
-            <Route exact path="/trading/:operation" component={Charge} />
+            <Route exact path="/trading/list" component={TradingList} />
+            <Route exact path="/trading/publish" component={TradingPublish} />
+            <Route exact path="/trading/buy/:id" component={TradingBuy} />
             <Route exact path="/history" component={History} />
             <Route exact path="/user" component={User} />
           </Switch>
