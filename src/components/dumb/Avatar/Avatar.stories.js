9806871eabb90749821bc616b480da3670caea47
Mariano <marianov994@gmail.com>
2020-01-26 04:11:49 -0300

    Avatar and Button with Knobs

--- src/components/dumb/Avatar/Avatar.stories.js
+++ src/components/dumb/Avatar/Avatar.stories.js
@@ -1,28 +1,14 @@
 import React from "react";
-// import { action } from "@storybook/addon-actions";
+import { storiesOf } from "@storybook/react";
+import { withKnobs, select } from "@storybook/addon-knobs";
 
 import SVGUser from "./../../../assets/components/SvgUser";
 import Avatar from "./Avatar";
 
-export default {
-  title: "Avatar",
-  component: Avatar
-};
-
-export const Small = () => (
-  <Avatar size="small">
-    <SVGUser />
-  </Avatar>
-);
-
-export const Medium = () => (
-  <Avatar size="medium">
-    <SVGUser />
-  </Avatar>
-);
-
-export const Big = () => (
-  <Avatar size="big">
-    <SVGUser />
-  </Avatar>
-);
+storiesOf("Dumb/Avatar", module)
+  .addDecorator(withKnobs)
+  .add("Avatar", () => (
+    <Avatar size={select("Size", ["small", "medium", "big"], "small")}>
+      <SVGUser />
+    </Avatar>
+  ));
--- src/components/dumb/Button/Button.stories.js
+++ src/components/dumb/Button/Button.stories.js
@@ -1,17 +1,19 @@
 import React from "react";
+import { storiesOf } from "@storybook/react";
+import { withKnobs, text, select } from "@storybook/addon-knobs";
 import { action } from "@storybook/addon-actions";
 
 import Button from "./Button";
 
-export default {
-  title: "Button",
-  component: Button
-};
-
-export const Text = () => (
-  <Button content="Hello!" disabled={false} onClick={action("clicked")} />
-);
-
-export const Emoji = () => (
-  <Button content={"😀 😎 👍 💯"} onClick={action("clicked")} />
-);
+storiesOf("Dumb/Button", module)
+  .addDecorator(withKnobs)
+  .add("Text", () => (
+    <Button
+      content={text("Name", "Mariano")}
+      disabled={select("Disabled", [true, false], true)}
+      onClick={action("clicked")}
+    />
+  ))
+  .add("Emoji", () => (
+    <Button content={"😀 😎 👍 💯"} onClick={action("clicked")} />
+  ));
