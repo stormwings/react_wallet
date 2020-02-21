f0470f95d1836be0f1f405fd4af6d3507c117142
Mariano Villarreal <marianov994@gmail.com>
2020-02-21 19:25:37 -0300

    Avatar component updated

--- src/components/dumb/Avatar/Avatar.stories.js
+++ src/components/dumb/Avatar/Avatar.stories.js
@@ -3,14 +3,12 @@ import { storiesOf } from "@storybook/react";
 import { withKnobs, select } from "@storybook/addon-knobs";
 import { jsxDecorator } from "storybook-addon-jsx";
 
-import SVGUser from "./../../../assets/components/SvgUser";
+import SVGUser from "./../../../assets/svg/user.svg";
 import Avatar from "./Avatar";
 
 storiesOf("Dumb/Avatar", module)
   .addDecorator(withKnobs)
   .addDecorator(jsxDecorator)
   .add("Avatar", () => (
-    <Avatar size={select("Size", ["small", "medium", "big"], "small")}>
-      <SVGUser />
-    </Avatar>
+    <Avatar image={SVGUser} size={select("Size", ["small", "medium", "big"], "small")} />
   ));
--- src/components/dumb/Avatar/Avatar.tsx
+++ src/components/dumb/Avatar/Avatar.tsx
@@ -1,25 +1,19 @@
-import React, { FunctionComponent } from "react";
-import "./Avatar.scss";
+import React, { FunctionComponent } from 'react';
+import './Avatar.scss';
 
 interface IProps {
-  image?: string;
-  children?: any;
-  size?: "small" | "medium" | "big";
+  id: string;
+  image: string;
+  size?: 'small' | 'medium' | 'big';
 }
 
-const Avatar: FunctionComponent<IProps> = ({ image, size, children }) => {
-  const className = `${size ? `image-${size}` : "image-small"}`;
-
-  if (children)
-    return (
-      <div id="avatar-image" className={className}>
-        {children}
-      </div>
-    );
-
-  return (
-    <img id="avatar-image" className={className} src={image} alt="avatar" />
-  );
-};
+const Avatar: FunctionComponent<IProps> = ({ image, size }) => (
+  <img 
+    id="avatar" 
+    className={`avatar --${size ? size : 'small'}`} 
+    src={image} 
+    alt="user avatar"
+  />
+);
 
 export default Avatar;
