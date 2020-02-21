205338ef405884dc26bff8fb0a98582a3c4b597c
Mariano Villarreal <marianov994@gmail.com>
2020-02-21 20:00:57 -0300

    cypress login test

--- cypress/integration/authentication/login.spec.js
+++ cypress/integration/authentication/login.spec.js
@@ -8,24 +8,25 @@ describe('Test authentication', () => {
     cy.visit('/auth/login');
   });
 
-  it('Must load login', () => {
+  it('User must login', () => {
     cy.get('@userData').then(userData => {
-      cy.get('#auth_username').type(userData.username);
-      cy.get('#auth_password').type(userData.wrong_password);
+      cy.get('#auth_input_username').type(userData.username);
+      cy.get('#auth_input_password').type(userData.password);
       cy.contains('.button', 'Confirm').click();
-
-      cy.contains('Unable to log in with provided credentials.').should('be.visible');
-      cy.get('#auth_password').clear();
-      cy.get('#auth_password').type(userData.password);
-      cy.contains('.button', 'Confirm').click();
-      cy.contains('Unable to log in with provided credentials.').should('not.exist');
+      cy.get('#auth_error_message').should('not.exist');
       cy.wait(2000);
+      cy.contains('Logout').click();
+      cy.url().should('include', '/auth/login')
     });
   });
 
-  after(() => {
-    cy.contains('Logout').click();
-    cy.url('/auth/login');
-    cy.log('Test finished');
+  it('Must fail login', () => {
+    cy.get('@userData').then(userData => {
+      cy.get('#auth_input_username').type(userData.username);
+      cy.get('#auth_input_password').type(userData.wrong_password);
+      cy.contains('.button', 'Confirm').click();
+
+      cy.get('#auth_error_message').should('be.visible');
+    });
   });
 });
