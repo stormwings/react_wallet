27d40a48707b2996d0d01db377be2c87c3d25759
Mariano Villarreal <marianov994@gmail.com>
2022-04-14 16:13:05 -0300

    clean not used libraries

--- cypress/support/commands.js
+++
@@ -1,5 +0,0 @@
-Cypress.Commands.add("completeLoginInput", (username, password) => {
-  cy.get('#auth_input_username').type(username);
-  cy.get('#auth_input_password').type(password);
-  cy.contains('.button', 'Confirm').click();
-})
