518bc5e40baa92353242280e5aad0c3ae6f44734
Mariano Villarreal <marianov994@gmail.com>
2022-04-14 21:03:54 -0300

    wallet reducer and actions mocked

--- src/redux/actions/walletActions.ts
+++ src/redux/actions/walletActions.ts
@@ -1,89 +1,110 @@
-import Axios from 'axios';
-import { Dispatch } from 'redux';
-import * as types from './../types';
-import * as urls from './../../helpers/api';
-import { buildTokenHeader } from './../../helpers/functions';
+// import Axios from 'axios'
+import { Dispatch } from 'redux'
+import * as types from './../types'
+// import * as urls from './../../helpers/api'
+// import { buildTokenHeader } from './../../helpers/functions'
 
-export const loadWallet = (payload: object) => ({ type: types.WALLET_LOAD, payload });
-export const updateWallet = (payload: object) => ({ type: types.WALLET_UPDATE, payload });
-export const errorWallet = (payload: object) => ({ type: types.WALLET_ERROR, payload });
-export const walletLoading = () => ({ type: types.WALLET_LOADING });
+import server from './../server.json'
+
+export const loadWallet = (payload: object) => ({
+    type: types.WALLET_LOAD,
+    payload,
+})
+
+export const updateWallet = (payload: object) => ({
+    type: types.WALLET_UPDATE,
+    payload,
+})
+
+export const errorWallet = (payload: object) => ({
+    type: types.WALLET_ERROR,
+    payload,
+})
+
+export const walletLoading = () => ({ type: types.WALLET_LOADING })
 
 export const fetchWallet = (userId: number) => async (dispatch: Dispatch) => {
-  dispatch(walletLoading());
-  try {
-    const headers = buildTokenHeader();
+    dispatch(walletLoading())
+    try {
+        // const headers = buildTokenHeader()
 
-    const wallet = await Axios.get(urls.URL_WALLET(userId), { headers });
-    const tradings = await Axios.get(urls.URL_TRADINGS, { headers });
-    const operations = await Axios.get(urls.URL_OPERATIONS, { headers });
+        // const wallet = await Axios.get(urls.URL_WALLET(userId), { headers })
+        // const tradings = await Axios.get(urls.URL_TRADINGS, { headers })
+        // const operations = await Axios.get(urls.URL_OPERATIONS, { headers })
 
-    await dispatch(loadWallet({ wallet, operations, tradings }));
-  } catch (error) {
-    const { message } = error;
-    dispatch(errorWallet({ message_error: message }));
-  }
-};
+        const wallet = server.wallet
+        const tradings = server.tradings
+        const operations = server.operation
 
-export const updateCurrency = (userId: number, body: any) => async (dispatch: Dispatch) => {
-  try {
-    const headers = buildTokenHeader();
+        await dispatch(loadWallet({ wallet, operations, tradings }))
+    } catch (error) {
+        const { message } = error as Error
+        dispatch(errorWallet({ message_error: message }))
+    }
+}
 
-    const { data } = await Axios.put(urls.URL_WALLET(userId), body, { headers });
-    dispatch(updateWallet(data));
-  } catch (error) {
-    const { message } = error;
-    dispatch(errorWallet({ message_error: message }));
-  }
-};
+export const updateCurrency =
+    (userId: number, body: any) => async (dispatch: Dispatch) => {
+        try {
+            // const headers = buildTokenHeader()
+
+            // const { data } = await Axios.put(urls.URL_WALLET(userId), body, {
+            //     headers,
+            // })
+
+            const wallet = server.wallet
+
+            dispatch(updateWallet(wallet))
+        } catch (error) {
+            const { message } = error as Error
+            dispatch(errorWallet({ message_error: message }))
+        }
+    }
 
 export const createOperation = (body: object) => async (dispatch: Dispatch) => {
-  try {
-    const headers = buildTokenHeader();
-
-    await Axios.post(urls.URL_OPERATIONS, body, { headers });
-  } catch (error) {
-    console.warn(error.config, error.code, error.request, error.response, error.isAxiosError);
-    const { message } = error;
-    dispatch(errorWallet({ message_error: message }));
-  }
-};
+    try {
+        // const headers = buildTokenHeader()
+        // await Axios.post(urls.URL_OPERATIONS, body, { headers })
+    } catch (error) {
+        const { message } = error as Error
+        dispatch(errorWallet({ message_error: message }))
+    }
+}
 
 export const createTrading = (body: object) => async (dispatch: Dispatch) => {
-  try {
-    const headers = buildTokenHeader();
+    try {
+        // const headers = buildTokenHeader()
+        // await Axios.post(urls.URL_TRADINGS, body, { headers })
+    } catch (error) {
+        const { message } = error as Error
 
-    await Axios.post(urls.URL_TRADINGS, body, { headers });
-  } catch (error) {
-    console.warn(error.config, error.code, error.request, error.response, error.isAxiosError);
-    const { message } = error;
-    dispatch(errorWallet({ message_error: message }));
-  }
-};
+        dispatch(errorWallet({ message_error: message }))
+    }
+}
 
-export const removeTrading = (tradingId: number) => async (dispatch: Dispatch) => {
-  try {
-    const headers = buildTokenHeader();
-    await Axios.delete(urls.URL_TRADING(tradingId), { headers });
-  } catch (error) {
-    console.warn(error.config, error.code, error.request, error.response, error.isAxiosError);
-    const { message } = error;
-    dispatch(errorWallet({ message_error: message }));
-  }
-};
+export const removeTrading =
+    (tradingId: number) => async (dispatch: Dispatch) => {
+        try {
+            // const headers = buildTokenHeader()
+            // await Axios.delete(urls.URL_TRADING(tradingId), { headers })
+        } catch (error) {
+            const { message } = error as Error
+            dispatch(errorWallet({ message_error: message }))
+        }
+    }
 
-export const updatePublisherCurrency = (publisherId: number, publisherMoney: number) => async (dispatch: Dispatch) => {
-  try {
-    const headers = buildTokenHeader();
-
-    let {
-      data: { USD }
-    } = await Axios.get(urls.URL_WALLET(publisherId), { headers });
-    USD += publisherMoney;
-    await Axios.put(urls.URL_WALLET(publisherId), { USD }, { headers });
-  } catch (error) {
-    console.warn(error.config, error.code, error.request, error.response, error.isAxiosError);
-    const { message } = error;
-    dispatch(errorWallet({ message_error: message }));
-  }
-};
+export const updatePublisherCurrency =
+    (publisherId: number, publisherMoney: number) =>
+    async (dispatch: Dispatch) => {
+        try {
+            // const headers = buildTokenHeader()
+            // let {
+            // data: { USD },
+            // } = await Axios.get(urls.URL_WALLET(publisherId), { headers })
+            // USD += publisherMoney
+            // await Axios.put(urls.URL_WALLET(publisherId), { USD }, { headers })
+        } catch (error) {
+            const { message } = error as Error
+            dispatch(errorWallet({ message_error: message }))
+        }
+    }
--- src/redux/reducers/walletReducer.ts
+++ src/redux/reducers/walletReducer.ts
@@ -1,38 +1,39 @@
-import { ResultOperation } from './../../entities/Operation';
-import CurrencyPrice from './../../entities/CurrencyPrice';
-import * as types from './../types';
+import { AnyAction } from 'redux'
+
+import { ResultOperation } from './../../entities/Operation'
+
+import { Trading, CurrencyPrice } from './../../entities'
+
+import * as types from './../types'
 
 const INITIAL_STATE = {
-  currency: {
-    BTC: 0,
-    USD: 0
-  } as CurrencyPrice,
-  operations: [] as Array<ResultOperation>,
-  tradings: [] as Array<any>
-};
+    currency: {
+        BTC: 0,
+        USD: 0,
+    } as CurrencyPrice,
+    operations: [] as ResultOperation[],
+    tradings: [] as Trading[],
+}
 
-export default function(state = INITIAL_STATE, action: any) {
-  switch (action.type) {
-    case types.WALLET_LOAD: {
-      const { wallet, operations, tradings } = action.payload;
+export default function (state = INITIAL_STATE, action: AnyAction) {
+    switch (action.type) {
+        case types.WALLET_LOAD: {
+            const { wallet, operations, tradings } = action.payload
 
-      operations.data.reverse();
-      tradings.data.reverse();
-
-      return {
-        currency: wallet.data,
-        operations: operations.data,
-        tradings: tradings.data
-      };
+            return {
+                currency: wallet,
+                operations: operations,
+                tradings: tradings,
+            }
+        }
+        case types.WALLET_UPDATE: {
+            const { BTC, USD } = action.payload
+            return {
+                ...state,
+                currency: { BTC, USD },
+            }
+        }
+        default:
+            return state
     }
-    case types.WALLET_UPDATE: {
-      const { BTC, USD } = action.payload;
-      return {
-        ...state,
-        currency: { BTC, USD }
-      };
-    }
-    default:
-      return state;
-  }
 }
